9826b07cf1f44011ab9c649b396c80a026f8ae83
Johan Mynhardt <johanmynhardt@gmail.com>
2015-11-30 15:42:39 +0200

    Make call to wordpress installation (TODO: mock)

--- src/test/java/com/afrozaar/wordpress/wpapi/v2/ClientTest.java
+++ src/test/java/com/afrozaar/wordpress/wpapi/v2/ClientTest.java
@@ -5,10 +5,14 @@ import com.afrozaar.wordpress.wpapi.v2.util.ClientFactory;
 
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Properties;
 
 public class ClientTest {
+
+    Logger LOG = LoggerFactory.getLogger(ClientTest.class);
 
     @Test
     public void foo() {
@@ -17,15 +21,22 @@ public class ClientTest {
 
     @Test
     public void posts() {
+
+
         Properties properties = new Properties();
         properties.put("baseUrl", "http://localhost/");
+        properties.put("username", "username");
+        properties.put("password", "password");
+
+        LOG.debug("properties: {}", properties);
 
         Client client = ClientFactory.fromProperties(properties);
 
         final PagedResponse<Post> postPagedResponse = client.fetchPosts();
 
-        // TODO
+        final long count = postPagedResponse.list.stream().count();
 
+        // TODO:
 
     }
 }
