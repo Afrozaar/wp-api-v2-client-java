8c0155357bde910be55a36b75944cea527c9c783
Freddie <freddieodonnell@afrozaar.com>
2015-12-01 17:47:15 +0200

    getting wiremock to work.

--- src/test/java/com/afrozaar/wordpress/wpapi/v2/ClientTest.java
+++ src/test/java/com/afrozaar/wordpress/wpapi/v2/ClientTest.java
@@ -6,21 +6,25 @@ import com.afrozaar.wordpress.wpapi.v2.model.Post;
 import com.afrozaar.wordpress.wpapi.v2.util.ClientConfig;
 import com.afrozaar.wordpress.wpapi.v2.util.ClientFactory;
 
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
 import com.google.common.collect.ImmutableMap;
 
+import org.junit.Rule;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
+import java.io.IOException;
+import java.net.*;
 import java.util.Arrays;
 
 public class ClientTest {
@@ -29,6 +33,9 @@ public class ClientTest {
     static ClientConfig clientConfig;
 
     Logger LOG = LoggerFactory.getLogger(ClientTest.class);
+
+    @Rule
+    public WireMockRule wireMockRule = new WireMockRule(8089);
 
     @BeforeClass
     public static void init() throws UnknownHostException {
@@ -86,5 +93,33 @@ public class ClientTest {
         final MultiValueMap<String, String> queryParams = UriComponentsBuilder.fromHttpUrl("http://foo.bar/?q=searchKey&v=moo").build().getQueryParams();
 
         LOG.debug("queryParams = {}", queryParams);
+    }
+
+    @Test
+    public void testWpEndpointForPosts_shouldReturnGivenPosts() {
+        stubFor(get(urlEqualTo("http://freddie-work/wp-json/wp/v2"))
+                .withHeader("Link", matching("<http://freddie-work/wp-json/wp/v2/posts?page=2>; rel=\"next\"")));
+    }
+
+    @Test
+    public void simpleStubTest_shouldReturnOk() {
+
+        int i = 0;
+
+        final Client client = ClientFactory.fromConfig(clientConfig);
+
+        stubFor(get(urlEqualTo("/some/thing"))
+                .willReturn(aResponse()
+                        .withStatus(200)));
+        try {
+            URL url = new URL("http://localhost:8089/some/thing");
+            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+            httpURLConnection.setRequestMethod("GET");
+            httpURLConnection.connect();
+            i = httpURLConnection.getResponseCode();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        assertThat(i).isEqualTo(200);
     }
 }
