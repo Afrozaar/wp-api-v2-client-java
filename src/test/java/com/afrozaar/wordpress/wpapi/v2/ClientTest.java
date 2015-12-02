99fa057da196ffc4f0c2698176415f29783b9017
Johan Mynhardt <johanmynhardt@gmail.com>
2015-12-02 11:53:18 +0200

    ClientTest cleanup and fixing imports.

--- src/test/java/com/afrozaar/wordpress/wpapi/v2/ClientTest.java
+++ src/test/java/com/afrozaar/wordpress/wpapi/v2/ClientTest.java
@@ -2,29 +2,42 @@ package com.afrozaar.wordpress.wpapi.v2;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import static org.junit.Assert.fail;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.matching;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+
 import com.afrozaar.wordpress.wpapi.v2.model.Post;
 import com.afrozaar.wordpress.wpapi.v2.util.ClientConfig;
 import com.afrozaar.wordpress.wpapi.v2.util.ClientFactory;
 
-import com.github.tomakehurst.wiremock.junit.WireMockRule;
 import com.google.common.collect.ImmutableMap;
+import com.google.common.io.ByteSource;
 
-import org.junit.Rule;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
 
 import org.junit.BeforeClass;
+import org.junit.Rule;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.net.*;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.InetAddress;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.UnknownHostException;
 import java.util.Arrays;
 
 public class ClientTest {
@@ -102,24 +115,38 @@ public class ClientTest {
     }
 
     @Test
-    public void simpleStubTest_shouldReturnOk() {
+    public void simpleStubTest_shouldReturnOk() throws MalformedURLException {
 
-        int i = 0;
+        // given
+        final String endpoint = "/some/thing";
+        final String body = "foo baz\nbar";
+        URL url = new URL("http://localhost:8089" + endpoint);
 
-        final Client client = ClientFactory.fromConfig(clientConfig);
-
-        stubFor(get(urlEqualTo("/some/thing"))
+        stubFor(get(urlEqualTo(endpoint))
                 .willReturn(aResponse()
+                        .withBody(body)
                         .withStatus(200)));
+
         try {
-            URL url = new URL("http://localhost:8089/some/thing");
+
+            // when
             HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
-            httpURLConnection.setRequestMethod("GET");
             httpURLConnection.connect();
-            i = httpURLConnection.getResponseCode();
+
+            // then
+            final int responseCode = httpURLConnection.getResponseCode();
+            assertThat(responseCode).isEqualTo(200);
+
+            ByteSource byteSource = new ByteSource() {
+                @Override
+                public InputStream openStream() throws IOException {
+                    return httpURLConnection.getInputStream();
+                }
+            };
+            final String output = new String(byteSource.read());
+            assertThat(output).isEqualTo(body);
         } catch (IOException e) {
-            e.printStackTrace();
+            fail(e.getMessage());
         }
-        assertThat(i).isEqualTo(200);
     }
 }
