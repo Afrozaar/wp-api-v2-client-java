f3a7e7527e907317adf948e1b987e63d0c4c0ce5
Johan Mynhardt <johanmynhardt@gmail.com>
2015-12-03 10:02:39 +0200

    Add a builder for SearchRequest.

--- src/main/java/com/afrozaar/wordpress/wpapi/v2/request/SearchRequest.java
+++ src/main/java/com/afrozaar/wordpress/wpapi/v2/request/SearchRequest.java
@@ -2,6 +2,7 @@ package com.afrozaar.wordpress.wpapi.v2.request;
 
 import com.google.common.collect.ImmutableMap;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
@@ -12,7 +13,41 @@ public class SearchRequest extends Request {
     }
 
     public static SearchRequest posts() {
-        return new SearchRequest(Request.POSTS, ImmutableMap.<String, List<String>>of());
+        return Builder.aSearchRequest().build();
     }
 
+    public static class Builder {
+        ImmutableMap.Builder<String, List<String>> paramBuilder = new ImmutableMap.Builder<>();
+        String uri = Request.POSTS;
+
+        private Builder() {
+        }
+
+        public static Builder aSearchRequest() {
+            return new Builder();
+        }
+
+        public Builder withParam(String key, String... values) {
+            paramBuilder.put(key, Arrays.asList(values));
+            return this;
+        }
+
+        public Builder withParams(Map<String, List<String>> params) {
+            this.paramBuilder.putAll(params);
+            return this;
+        }
+
+        public Builder withUri(String uri) {
+            this.uri = uri;
+            return this;
+        }
+
+        public Builder but() {
+            return aSearchRequest().withParams(paramBuilder.build()).withUri(uri);
+        }
+
+        public SearchRequest build() {
+            return new SearchRequest(uri, paramBuilder.build());
+        }
+    }
 }
